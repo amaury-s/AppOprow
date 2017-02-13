35f4309ff4a72246f88a3818fddb6f2bca12db79
batmorell <baptiste.morellab@gmail.com>
2017-02-13 19:22:13 +0100

    Adding page

---
+++ src/pages/page-mes-infos/page-mes-infos.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/*
+  Generated class for the PageMesInfos page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-page-mes-infos',
+  templateUrl: 'page-mes-infos.html'
+})
+export class PageMesInfosPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PageMesInfosPage');
+  }
+
+}

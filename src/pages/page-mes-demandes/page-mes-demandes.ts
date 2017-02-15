66067e12d999cda7277358d70c632378c3a3ebf1
Sacha Kleynjans <sacha.kleynjans@gmail.com>
2017-02-15 12:21:07 +0100

    Mes demandes page : content update

--- src/pages/page-mes-demandes/page-mes-demandes.ts
+++ src/pages/page-mes-demandes/page-mes-demandes.ts
@@ -13,6 +13,10 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class PageMesDemandes {
 
+    public demandes = [
+        {name: 'Mairie 14', need:'Refaire passeport', date:'Jeudi 32 Janvier', time:'14h30'},
+        {name: 'Mairie 15', need: 'Manger une pomme', date:'Lundi 3 Mai', time:'9h17'}];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
